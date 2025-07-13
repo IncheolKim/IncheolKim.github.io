19184cbebe3ae4057be43712d16278ba5b8d61ab
IncheolKim <8827880+IncheolKim@users.noreply.github.com>
2025-07-13 05:01:30 +0000

    Deploying to gh-pages from @ IncheolKim/IncheolKim.github.io@e9c19096070a535f3cadbaf6c9659768ae108d55 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,16 +35,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-nlos-paper-has-been-accepted-to-siggraph-asia-2023",
+          title: 'Our NLOS paper has been accepted to SIGGRAPH Asia 2023.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
